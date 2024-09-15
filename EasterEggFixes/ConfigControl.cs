3df38f5856dc7233a45c75e9df53b86cd1c6c9b8
Spookybuddy <jliving3@student.fitchburgstate.edu>
2024-09-15 13:39:07 -0400

    Easter Egg Fixes 2.0
    Added Config settings to Easter Eggs
    Can set Eggs to Always, Sometimes, Never explode!
    Fixed Desync issues!

---
+++ EasterEggFixes/ConfigControl.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx.Configuration;
+
+namespace EggFixes
+{
+    class ConfigControl : SyncedInstance<ConfigControl>
+    {
+        public ConfigEntry<bool> cfgEnabled;
+        public ConfigEntry<EasterEggFixesModBase.EggSettings> cfgExplodeWhen;
+        public ConfigEntry<int> cfgChance;
+
+        internal bool Enabled
+        {
+            get
+            {
+                if (cfgEnabled != null) return cfgEnabled.Value;
+                return true;
+            }
+            set => cfgEnabled.Value = value;
+        }
+
+        internal EasterEggFixesModBase.EggSettings ExplodeWhen
+        {
+            get
+            {
+                if (cfgExplodeWhen == null) return (EasterEggFixesModBase.EggSettings)cfgExplodeWhen.DefaultValue;
+                return cfgExplodeWhen.Value;
+            }
+            set => cfgExplodeWhen.Value = value;
+        }
+
+        internal int Chance
+        {
+            get
+            {
+                if (cfgChance.Value > 0 && cfgChance.Value < 100) return cfgChance.Value;
+                return (int)cfgChance.DefaultValue;
+            }
+            set => cfgChance.Value = value;
+        }
+
+        public ConfigControl(ConfigFile cfg)
+        {
+            InitInstance(this);
+            cfgEnabled = cfg.Bind("Eggs", "Enabled", true, "Turn the mod on.");
+            cfgExplodeWhen = cfg.Bind("Eggs", "Explode When", EasterEggFixesModBase.EggSettings.ExplodeOnThrow, "When should the eggs explode?");
+            cfgChance = cfg.Bind("Eggs", "Chance to Explode", 16, "The percent chance for the eggs to explode when set to 'ChanceToExplode'. \nRanges from 1 - 99");
+        }
+    }
+}
---
+++ EasterEggFixes/SyncedInstance.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Unity.Netcode;
+using Unity.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using HarmonyLib;
+
+namespace EggFixes
+{
+    [Serializable]
+    public class SyncedInstance<T>
+    {
+        internal static CustomMessagingManager MessageManager => NetworkManager.Singleton.CustomMessagingManager;
+        internal static bool IsClient => NetworkManager.Singleton.IsClient;
+        internal static bool IsHost => NetworkManager.Singleton.IsHost;
+
+        [NonSerialized]
+        protected static int IntSize = 4;
+
+        public static T Default { get; private set; }
+        public static T Instance { get; private set; }
+
+        public static bool Synced { get; internal set; }
+
+        //Initialization
+        protected void InitInstance(T instance)
+        {
+            Default = instance;
+            Instance = instance;
+            IntSize = sizeof(int);
+        }
+
+        internal static void SyncInstance(byte[] data)
+        {
+            Instance = DeserializeFromBytes(data);
+            Synced = true;
+        }
+
+        internal static void RevertSync()
+        {
+            Instance = Default;
+            Synced = false;
+        }
+
+        public static byte[] SerializeToBytes(T val)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream stream = new MemoryStream();
+
+            try {
+                bf.Serialize(stream, val);
+                return stream.ToArray();
+            } catch (Exception e) {
+                EasterEggFixesModBase.mls.LogError($"Error serializing instance: {e}");
+                return null;
+            }
+        }
+
+        public static T DeserializeFromBytes(byte[] data)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream stream = new MemoryStream(data);
+
+            try {
+                return (T)bf.Deserialize(stream);
+            } catch (Exception e) {
+                EasterEggFixesModBase.mls.LogError($"Error deserializing instance: {e}");
+                return default;
+            }
+        }
+
+        //Syncing
+        public static void RequestSync()
+        {
+            if (!IsClient) return;
+
+            FastBufferWriter stream = new FastBufferWriter(IntSize, Allocator.Temp);
+            MessageManager.SendNamedMessage("SpikeTrapFixes_OnRequestConfigSync", 0uL, stream);
+        }
+
+        public static void OnRequestSync(ulong clientId, FastBufferReader _)
+        {
+            if (!IsHost) return;
+
+            EasterEggFixesModBase.mls.LogInfo($"Config sync request received from client: {clientId}");
+
+            byte[] array = SerializeToBytes(Instance);
+            int value = array.Length;
+
+            FastBufferWriter stream = new FastBufferWriter(value + IntSize, Allocator.Temp);
+
+            try {
+                stream.WriteValueSafe(in value, default);
+                stream.WriteBytesSafe(array);
+
+                MessageManager.SendNamedMessage("SpikeTrapFixes_OnReceiveConfigSync", clientId, stream);
+            } catch (Exception e) {
+                EasterEggFixesModBase.mls.LogInfo($"Error occurred syncing config with client: {clientId}\n{e}");
+            }
+        }
+
+        public static void OnReceiveSync(ulong _, FastBufferReader reader)
+        {
+            if (!reader.TryBeginRead(IntSize)) {
+                EasterEggFixesModBase.mls.LogError("Config sync error: Could not begin reading buffer.");
+                return;
+            }
+
+            reader.ReadValueSafe(out int val, default);
+            if (!reader.TryBeginRead(val)) {
+                EasterEggFixesModBase.mls.LogError("Config sync error: Host could not sync.");
+                return;
+            }
+
+            byte[] data = new byte[val];
+            reader.ReadBytesSafe(ref data, val);
+
+            SyncInstance(data);
+
+            EasterEggFixesModBase.mls.LogInfo("Successfully synced config with host.");
+        }
+
+        //Patches
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(GameNetworkManager), "SteamMatchmaking_OnLobbyMemberJoined")]
+        public static void InitializeLocalPlayer()
+        {
+            if (IsHost) {
+                MessageManager.RegisterNamedMessageHandler("SpikeTrapFixes_OnRequestConfigSync", OnRequestSync);
+                Synced = true;
+
+                return;
+            }
+
+            Synced = false;
+            MessageManager.RegisterNamedMessageHandler("SpikeTrapFixes_OnReceiveConfigSync", OnReceiveSync);
+            RequestSync();
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(GameNetworkManager), "StartDisconnect")]
+        public static void PlayerLeave()
+        {
+            ConfigControl.RevertSync();
+        }
+    }
+}
