e7919c2f93f75631a596d6fe03d52f1e4b68e41f
Spookybuddy <93450263+Spookybuddy@users.noreply.github.com>
2025-03-26 11:46:32 -0400

    MI & STF patch updates
    Large patch update for MI
    Highly requested fix added to STF

---
+++ MapImprovements/CustomKillTrigger.cs
@@ -0,0 +1,15 @@
+﻿using GameNetcodeStuff;
+using Unity.Netcode;
+using UnityEngine;
+
+public class CustomKillTrigger : NetworkBehaviour
+{
+    void OnTriggerStay(Collider other)
+    {
+        if (other.TryGetComponent<PlayerControllerB>(out PlayerControllerB component)) {
+            if (component == GameNetworkManager.Instance.localPlayerController && !component.isPlayerDead) {
+                GameNetworkManager.Instance.localPlayerController.KillPlayer(Vector3.down, true, CauseOfDeath.Gravity);
+            }
+        }
+    }
+}
